0d6367c91d8c1bc8ba7d89a3f4c12b15683cb626
219020140 <219020140@MBO_ICT102_09-PC.ump.ac.za>
2020-08-24 01:26:38 +0200

    My First Commit

---
+++ src/nkadimeng/pkg201627930/Nkadimeng201627930.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nkadimeng.pkg201627930;
+
+/**
+ *
+ * @author 219020140
+ */
+public class Nkadimeng201627930 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
